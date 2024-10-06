1b52058f9d17c5c90af9d2f6729000a9b8d6e58c
Eduardo Forero <eduardo.forero.r@gmail.com>
2024-10-05 19:14:12 -0500

    The logic for light/dark mode functionality and its corresponding styles has been added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,24 @@
+import { useState, useEffect } from 'react';
 import { Logo, DarkIcon, LightIcon } from './Icons';
 import './Nav.css';
 
 const Nav = () => {
+
+    const [theme, setTheme] = useState('light');
+
+    const handleTheme = (e) => setTheme(e.target.checked ? 'dark' : 'light');
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme)
+    }, [theme]);
+
     return (
         <nav>
             <Logo />
             <div className="light-dark-switcher">
                 <LightIcon />
                 <label>
-                    <input type="checkbox" className="light-dark-toggle" hidden />
+                    <input type="checkbox" className="light-dark-toggle" onChange={handleTheme} hidden />
                     <span className="light-dark-slider"></span>
                 </label>
                 <DarkIcon />
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -5,12 +5,12 @@ function Searchbar({ search, setSearch, searchPokemon }) {
     return (
         <>
         <section className="searchbar">
-        <h1 className="title">Search for your favorite Pokémon</h1>
+        <h1 className="title">Find your favorite Pokémon <br />there are more than 1,000 of them</h1>
             <form className="searchbar-form" onSubmit={searchPokemon}>
                 <fieldset>
                     <legend className="visually-hidden">Search Pokémon</legend>
                     <label htmlFor="search-input" className="visually-hidden">Search for a Pokémon</label>
-                    <input type="text" id="search-input" className="search-input" placeholder="Search for a Pokémon" value={search} onChange={(e) => setSearch(e.target.value)} />
+                    <input type="text" id="search-input" className="search-input" placeholder="Search for a Pokémon by its ID or Name" value={search} onChange={(e) => setSearch(e.target.value)} />
                     <button type="submit" className="search-button">
                         <SearchIcon />
                     </button>
